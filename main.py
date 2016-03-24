ce4c16aba6ce79f5df15e34616a97d8f620fba3b
jakobkogler <jakob.kogler@gmail.com>
2016-03-24 19:31:18 +0100

    Lots of docstrings

--- main.py
+++ main.py
@@ -9,20 +9,25 @@ from reciter import Reciter
 
 
 class RootWidget(FloatLayout):
+    """Root widget for the app."""
     pass
 
 
 class ReciteScreen(Screen):
+    """Screen for reciting pi"""
+
     pi_output = StringProperty('3.')
     correct_digits = StringProperty('')
 
     def __init__(self, **kwargs):
+        """Initialize everything and start calculating the first digits of pi."""
         super(ReciteScreen, self).__init__(**kwargs)
         self.reciter = Reciter()
         self.image = None
         self.update_correct_digits()
 
     def digit_pressed(self, digit):
+        """After pressing a digit on the screen, verify it and react to the result."""
         if not self.image:
             if self.reciter.check_next_digit(digit):
                 self.pi_output += digit
@@ -33,25 +38,32 @@ class ReciteScreen(Screen):
                 self.animate_grow(self.image)
 
     def update_correct_digits(self):
+        """Display the current count of correct recited digits of pi."""
         self.correct_digits = 'Correct digits: {}'.format(self.reciter.pos)
 
     def animate_grow(self, instance):
+        """Make an animation that grows an image from 0 to 100."""
         animation = Animation(size_hint=(1, 1), duration=.5)
         animation.bind(on_complete=self.remove_wrong_digit_image)
         animation.start(instance)
 
     def remove_wrong_digit_image(self, *_):
+        """Remove the image, that is displayed when a wrong digits was inputted."""
         self.remove_widget(self.image)
         self.image = None
 
 
 class WrongDigitImage(Image):
+    """"Class representing the image 'X.png', that is displayed after a wrong input."""
     pass
 
 
 class MainApp(App):
+    """Class representing the app."""
+
     @staticmethod
     def build():
+        """"Builds the app by creating a root widget."""
         return RootWidget()
 
 
--- reciter.py
+++ reciter.py
@@ -1,7 +1,11 @@
 from pi_memorize.compute_pi import ComputePi
 
+
 class Reciter:
+    """Calculates pi and validates inputted digits."""
+
     def __init__(self):
+        """Setups all variables and starts computing pi."""
         self.computer = ComputePi()
         self.current_calculated = 0
         self.pi = ''
@@ -9,17 +13,19 @@ class Reciter:
         self.compute_pi(100)
 
     def reset(self):
+        """Sets the number of correct digits back to zero."""
         self.pos = 0
 
     def check_next_digit(self, digit):
+        """Checks, if the next inputted digit is correct."""
         if digit == self.pi[self.pos]:
             self.pos += 1
             if self.pos == self.current_calculated:
                 self.compute_pi(self.current_calculated + 100)
             return True
-
         return False
 
-    def compute_pi(self, count):
-        self.pi = self.computer.BBP(count)[2:]
-        self.current_calculated = count
+    def compute_pi(self, precision):
+        """Computes the digits of pi."""
+        self.pi = self.computer.BBP(precision)[2:]
+        self.current_calculated = precision
